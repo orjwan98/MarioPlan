ab4187af88062392c74422dc9d5d17325c33c4c0
Orjwan <orjwan98@hotmail.com>
2019-09-03 22:54:23 +0300

    import login and signup and set up routes for both of them

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
-import Navbar from './components/layout/Navbar'; 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import Login from './components/auth/Login';
+import SignUp from './components/auth/Signup';
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" component={Dashboard} />
-          <Route path="/projects/:id" component={ProjectDetails}/>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/projects/:id" component={ProjectDetails} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
