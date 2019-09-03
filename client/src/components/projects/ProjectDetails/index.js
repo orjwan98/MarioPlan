0785855353309ca11822e91c516fa2d96b4262dc
Orjwan <orjwan98@hotmail.com>
2019-09-03 21:43:29 +0300

    build ProjectDetails component

---
+++ client/src/components/projects/ProjectDetails/index.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <h3 className="card-title">Project Title</h3>
+                    <p>lorem epsum</p>
+                </div>
+                <div className="card-action grey ligten-darken-4 grey-text">
+                    <p>
+                        Posted by the net ninja
+                    </p>
+                     <p>
+                        3rd September, 2am
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails;
