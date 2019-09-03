9ff834681e5319c511489caabd0015a5c798298f
Orjwan <orjwan98@hotmail.com>
2019-09-03 22:51:45 +0300

    extract id from params and add it to project title

--- client/src/components/projects/ProjectDetails/index.js
+++ client/src/components/projects/ProjectDetails/index.js
@@ -1,14 +1,15 @@
 import React from 'react'; 
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    const {id} = props.match.params
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <h3 className="card-title">Project Title</h3>
+                    <h3 className="card-title">Project Title - {id}</h3>
                     <p>lorem epsum</p>
                 </div>
-                <div className="card-action grey ligten-darken-4 grey-text">
+                <div className="card-action ligten-darken-4 grey-text">
                     <p>
                         Posted by the net ninja
                     </p>
